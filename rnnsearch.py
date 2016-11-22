96b986724697e34e346a1eaea8becee24ebb70b5
Playinf <playinf@stu.xmu.edu.cn>
2016-11-22 21:11:43 +0800

    bug fixup

--- rnnsearch.py
+++ rnnsearch.py
@@ -612,7 +612,7 @@ def train(args):
                         serialize(bestname, model)
 
             if count % option["sfreq"] == 0:
-                n = len(data)
+                n = len(data[0])
                 ind = numpy.random.randint(0, n)
                 sdata = data[0][ind]
                 tdata = data[1][ind]
